333c3315b40fbd1c47f1139218bf0ec3d875bbce
jlamothe7 <jeffreylamothe@gmail.com>
2020-02-17 19:23:57 -0500

    added a new package for hand checker

---
+++ src/main/java/com/jeffrey/project/card/handrank/HandChecker.java
@@ -0,0 +1,33 @@
+package com.jeffrey.project.card.handrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jeffrey.project.card.model.GameState;
+import com.jeffrey.project.card.model.player.Player;
+
+@Component
+public class HandChecker {
+
+	@Autowired 
+	GameState gameState;
+	
+	public List<Player> determineWinner() {
+		// add the winner(s) to this list and return it at the end: 
+		List<Player> winnerList = new ArrayList<Player>();
+		
+		/*
+		 * 
+		 * hand check code goes right here 
+		 * 
+		 * 
+		 */
+		
+		
+		return winnerList; 
+	}
+	
+}
