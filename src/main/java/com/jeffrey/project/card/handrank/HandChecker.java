a050bef5865431fa50174df5d8afc8710d53d841
John Zorc <johnny.zorc@gmail.com>
2020-05-19 20:59:07 -0400

    More Changes to HandChecker.Java

--- src/main/java/com/jeffrey/project/card/handrank/HandChecker.java
+++ src/main/java/com/jeffrey/project/card/handrank/HandChecker.java
@@ -1,3 +1,12 @@
+/*
+ * AUTHOR: John Zorc
+ * Date: May 18 2020
+ * Email: johnny.zorc@gmail.com
+ * 
+ * Description: The purpose of this class is to provide the determine winner function
+ * that will return a list of lists of all the players. The player/players in the first
+ * list will be the winner/winners, the lists are order from best hands to worst
+ */
 package com.jeffrey.project.card.handrank;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,20 +26,30 @@ public class HandChecker {
 	GameState gameState;
 			
 	//Returns a list of players in the order that they are ranked
-	public List<Player> determineWinner() {
+	public ArrayList<ArrayList<Player>> determineWinner() {
 		
 		List<Player> allPlayers = gameState.getAllPlayers();//Player at index 0 should be Master
+		ArrayList<ArrayList<Player>> rankingsList = new ArrayList<ArrayList<Player>>();
 		int numPlayers = allPlayers.size();
 		int[] handRankings = new int[numPlayers];
-		
+		int currBestHandVal; //Used to make list of lists
+		int numOfTiedHands = 0;
+		int currList = 0; 
+		int currHandIndex = 0; //Use to keep track of insertions into list of list
 		Player currPlayer;
 		
-		//Give each player their hand rank where the i'th index corresponds to the ith index in allplayers list
+		//Initiate lists of lists
+		for(int i = 0; i < numPlayers; i++) {
+			rankingsList.add(new ArrayList<Player>());
+		}
+		
+		//Give each player their hand rank where the i'th index corresponds to the i'th index in allPlayers list
 		for(int i = 0; i < numPlayers; i++) {
 			currPlayer = allPlayers.get(i); 
 			handRankings[i] = handChecker(currPlayer.getHand());
 		}
  
+		//Order the players by types of hands best to worst(Ties not checked here)
 		for(int i = 1; i < numPlayers; i++) {
 			for(int j = i; j > 0 ; j--) {
 				if(handRankings[j] < handRankings[j - 1]) {
@@ -44,7 +63,57 @@ public class HandChecker {
 			}
 		}
 		
-		return allPlayers; 
+		//Check for duplicate values and determine if tie, add to the rankingsList
+		currBestHandVal = handRankings[0];
+		for(int i = 0; i < numPlayers; i++) {
+			if(handRankings[i] == currBestHandVal) {
+				numOfTiedHands++;
+			}
+			else {
+				//No tie, just insert the one player into their own list
+				if(numOfTiedHands == 1) {
+					rankingsList.get(currList).set(0, allPlayers.get(currHandIndex));
+					currHandIndex++;
+					numOfTiedHands = 0;
+					currList++;
+					currBestHandVal = handRankings[i];
+				}
+				//There is a tie, rank the ties then insert based in the rank of the ties
+				else {
+					ArrayList<Hand> tiedHands = new ArrayList<Hand>();
+					int handsInserted = 0;
+					int[] handValues = new int[numOfTiedHands];
+					
+					//Add all the hands of the tied hand type to an array
+					for(int j = 0; j < numOfTiedHands; j++) {
+						tiedHands.add(allPlayers.get(currHandIndex + j).getCurrentHand());
+					}
+					
+					//Return values starting at 1 that increase when the hand gets worse
+					//So all hands with value == 1 are tied as best hands of that hand type
+					handValues = tieChecker(tiedHands, currBestHandVal);
+					int currTieVal = 1;
+					int playersInsertedInList = 0;
+					while(handsInserted < numOfTiedHands) {
+						//Insert all hands of tied value into a list then ++ list, and the currTieVal
+						for(int j = 0; j < numOfTiedHands; j++) {
+							if(handValues[j] == currTieVal) {
+								rankingsList.get(currList).set(playersInsertedInList, allPlayers.get(currHandIndex + j));
+								playersInsertedInList++;
+								handsInserted++;
+							}	
+						}
+						playersInsertedInList = 0;
+						currList++;
+						currTieVal++;
+					}
+					currHandIndex = currHandIndex + numOfTiedHands;
+					numOfTiedHands = 0;
+					currBestHandVal = handRankings[i];
+				}
+			}
+		}
+		return rankingsList; 
 	}
 	
 	
@@ -93,6 +162,51 @@ public class HandChecker {
 		}
 		else
 			return 10;
+	}
+	
+	//This function will take in an array of hands and a tie specifier number  
+	//RETURN ARRAY NUMBERS ASSOCIATED WITH TIED HAND RANK WHERE 1 IS BEST THEN 2...
+	public int[] tieChecker(ArrayList<Hand> tiedHands, int tieNumber) {
+		int numOfTiedHands = tiedHands.size();
+		ArrayList<Card> communityCards = new ArrayList<Card>();
+		int[] handValues = new int[numOfTiedHands];
+		
+		communityCards.add(gameState.getRiver());
+		communityCards.add(gameState.getTurn());
+		communityCards.add(gameState.getFlop().get(0));
+		communityCards.add(gameState.getFlop().get(1));
+		communityCards.add(gameState.getFlop().get(2));
+		
+		if(tieNumber == 1) {
+			for(int i = 0; i < numOfTiedHands; i++) {
+				handValues[i] = 1;
+			}
+		}
+		else if(tieNumber == 2) {
+			handValues = straitFlushTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 3) {
+			handValues = quadsTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 4) {
+			handValues = fullHouseTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 5) {
+			handValues = flushTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 6) {
+			handValues = straitTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 7) {
+			handValues = tripsTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 8) {
+			handValues = twoPairTieRanker(tiedHands, communityCards);
+		}
+		else if(tieNumber == 9) {
+			handValues = pairTieRanker(tiedHands, communityCards);
+		}
+		return handValues;
 	}
 	
 	public boolean isRoyalFlush(ArrayList<Card> playableCards) {
@@ -266,22 +380,93 @@ public class HandChecker {
 	}
 	public boolean isPair(ArrayList<Card> playableCards) {
 		int currNumber;
-		int count = 0;
 		
 		for(int i = 0; i < playableCards.size(); i++) {
 			currNumber = playableCards.get(i).getNumber();
 			for(int j = i + 1; j < playableCards.size(); j ++){
 				if(playableCards.get(j).getNumber() == currNumber) {
-					count++;
+					return true;
 				}
 			}
-			if(count == 2) {
-				return true;
-			}
-			else 
-				count = 0;
 		}
 		return false;
+	}
+	
+	public int[] straitFlushTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] quadsTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] fullHouseTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] flushTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] straitTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] tripsTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] twoPairTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int[] tiedHandRankings = new int[tiedHands.size()];
+		return tiedHandRankings;
+	}
+	public int[] pairTieRanker(ArrayList<Hand> tiedHands, ArrayList<Card> communityCards) {
+		int numOfTiedHands = tiedHands.size();
+		int[] tiedHandRankings = new int[numOfTiedHands];
+		int[] highPair = new int[numOfTiedHands];
+		int currNum;
+		int lastHighestPair = 15; 
+		int inserted = 0;
+		int currHandRanking = 1;
+		
+		for(int i = 0; i < numOfTiedHands; i++) {
+			int currHighPair = 0;
+			ArrayList<Card> currPlayableCards = new ArrayList<Card>();
+			currPlayableCards.add(tiedHands.get(i).getCardA());
+			currPlayableCards.add(tiedHands.get(i).getCardB());
+			currPlayableCards.add(communityCards.get(0));
+			currPlayableCards.add(communityCards.get(1));
+			currPlayableCards.add(communityCards.get(2));
+			currPlayableCards.add(communityCards.get(3));
+			currPlayableCards.add(communityCards.get(4));
+			
+			for(int j = 0; j < currPlayableCards.size(); j++) {
+				currNum = currPlayableCards.get(j).getNumber();
+				for(int k = j + 1; k < currPlayableCards.size(); k++) {
+					if(currPlayableCards.get(k).getNumber() == currNum && currNum >= currHighPair)
+						currHighPair = currNum;
+				}
+			}
+			highPair[i] = currHighPair;
+		}
+		while(inserted < numOfTiedHands) {
+			int tempHigh = 0;
+			for(int i = 0; i < numOfTiedHands; i++) {
+				if(highPair[i] >= tempHigh && highPair[i] < lastHighestPair) {
+					tempHigh = highPair[i];
+				}
+			}
+			for(int i = 0; i < numOfTiedHands; i++) {
+				if(highPair[i] == tempHigh) {
+					tiedHandRankings[i] = currHandRanking;
+					inserted++;
+				}
+			}
+			currHandRanking++;
+			lastHighestPair = tempHigh; 
+		}
+		
+		return tiedHandRankings;
 	}
 }
 
